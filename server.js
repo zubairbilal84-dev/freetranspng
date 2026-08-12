4566f2df65a63b9b3d0e6f9972c5a034eb8566ec
Zubair Bilal <zubairbilal84@gmail.com>
2026-08-12 18:32:57 +0530

    fix firebase env config

--- server.js
+++ server.js
@@ -23,7 +23,11 @@ app.use(session({
 
 // Firebase Admin Initialization
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined
+  }),
   storageBucket: 'freetranspng.firebasestorage.app'
 });
 
