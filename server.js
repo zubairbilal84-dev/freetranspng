f5464a469cf4d4da0a11c67be0e6ee6a1644ca28
Zubair Bilal <zubairbilal84@gmail.com>
2026-08-13 13:50:45 +0530

    use secret file for firebase

--- server.js
+++ server.js
@@ -21,16 +21,12 @@ app.use(session({
 }));
 
 // Firebase Admin Initialization using Environment Variables
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined
-    }),
-    storageBucket: 'freetranspng.firebasestorage.app'
-  });
-}
+const serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: 'freetranspng.firebasestorage.app'
+});
 
 const bucket = admin.storage().bucket();
 
